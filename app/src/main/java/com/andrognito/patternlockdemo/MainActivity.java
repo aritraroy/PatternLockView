cb053a412a10301663859f166447ae0500afab46
Aritra Roy <aritra.roy.in@gmail.com>
2017-04-03 13:33:32 +0530

    Added logo, improved sample

--- app/src/main/java/com/andrognito/patternlockdemo/MainActivity.java
+++ app/src/main/java/com/andrognito/patternlockdemo/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.WindowManager;
 import com.andrognito.patternlockview.PatternLockView;
 import com.andrognito.patternlockview.listener.PatternLockViewListener;
 import com.andrognito.patternlockview.utils.PatternLockUtils;
+import com.andrognito.patternlockview.utils.ResourceUtils;
 import com.andrognito.rxpatternlockview.RxPatternLockView;
 import com.andrognito.rxpatternlockview.events.PatternLockCompleteEvent;
 import com.andrognito.rxpatternlockview.events.PatternLockCompoundEvent;
@@ -21,6 +22,30 @@ public class MainActivity extends AppCompatActivity {
 
     private PatternLockView mPatternLockView;
 
+    private PatternLockViewListener mPatternLockViewListener = new PatternLockViewListener() {
+        @Override
+        public void onStarted() {
+            Log.d(getClass().getName(), "Pattern drawing started");
+        }
+
+        @Override
+        public void onProgress(List<PatternLockView.Dot> progressPattern) {
+            Log.d(getClass().getName(), "Pattern progress: " +
+                    PatternLockUtils.patternToString(mPatternLockView, progressPattern));
+        }
+
+        @Override
+        public void onComplete(List<PatternLockView.Dot> pattern) {
+            Log.d(getClass().getName(), "Pattern complete: " +
+                    PatternLockUtils.patternToString(mPatternLockView, pattern));
+        }
+
+        @Override
+        public void onCleared() {
+            Log.d(getClass().getName(), "Pattern has been cleared");
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,33 +56,19 @@ public class MainActivity extends AppCompatActivity {
 
         mPatternLockView = (PatternLockView) findViewById(R.id.patter_lock_view);
         mPatternLockView.setDotCount(3);
+        mPatternLockView.setDotNormalSize((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_dot_size));
+        mPatternLockView.setDotSelectedSize((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_dot_selected_size));
+        mPatternLockView.setPathWidth((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_path_width));
         mPatternLockView.setAspectRatioEnabled(true);
         mPatternLockView.setAspectRatio(PatternLockView.AspectRatio.ASPECT_RATIO_HEIGHT_BIAS);
         mPatternLockView.setViewMode(PatternLockView.PatternViewMode.CORRECT);
         mPatternLockView.setDotAnimationDuration(150);
-        mPatternLockView.addPatternLockListener(new PatternLockViewListener() {
-            @Override
-            public void onStarted() {
-                Log.d(getClass().getName(), "Pattern Started");
-            }
-
-            @Override
-            public void onProgress(List<PatternLockView.Dot> progressPattern) {
-                Log.d(getClass().getName(), "Pattern Progress: " +
-                        PatternLockUtils.patternToString(mPatternLockView, progressPattern));
-            }
-
-            @Override
-            public void onComplete(List<PatternLockView.Dot> pattern) {
-                Log.d(getClass().getName(), "Pattern Complete: " +
-                        PatternLockUtils.patternToString(mPatternLockView, pattern));
-            }
-
-            @Override
-            public void onCleared() {
-                Log.d(getClass().getName(), "Pattern Cleared");
-            }
-        });
+        mPatternLockView.setPathEndAnimationDuration(100);
+        mPatternLockView.setCorrectStateColor(ResourceUtils.getColor(this, R.color.white));
+        mPatternLockView.setInStealthMode(true);
+        mPatternLockView.setTactileFeedbackEnabled(true);
+        mPatternLockView.setInputEnabled(false);
+        mPatternLockView.addPatternLockListener(mPatternLockViewListener);
 
         RxPatternLockView.patternComplete(mPatternLockView)
                 .subscribe(new Consumer<PatternLockCompleteEvent>() {
@@ -70,11 +81,17 @@ public class MainActivity extends AppCompatActivity {
         RxPatternLockView.patternChanges(mPatternLockView)
                 .subscribe(new Consumer<PatternLockCompoundEvent>() {
                     @Override
-                    public void accept(PatternLockCompoundEvent patternLockEvent) throws Exception {
-                        if (patternLockEvent.getEventType() == PatternLockCompoundEvent.EventType.PATTERN_COMPLETE
-                                && patternLockEvent.getPattern() != null) {
-                            Log.d(getClass().getName(), PatternLockUtils.patternToString(mPatternLockView,
-                                    patternLockEvent.getPattern()));
+                    public void accept(PatternLockCompoundEvent event) throws Exception {
+                        if (event.getEventType() == PatternLockCompoundEvent.EventType.PATTERN_STARTED) {
+                            Log.d(getClass().getName(), "Pattern drawing started");
+                        } else if (event.getEventType() == PatternLockCompoundEvent.EventType.PATTERN_PROGRESS) {
+                            Log.d(getClass().getName(), "Pattern progress: " +
+                                    PatternLockUtils.patternToString(mPatternLockView, event.getPattern()));
+                        } else if (event.getEventType() == PatternLockCompoundEvent.EventType.PATTERN_COMPLETE) {
+                            Log.d(getClass().getName(), "Pattern complete: " +
+                                    PatternLockUtils.patternToString(mPatternLockView, event.getPattern()));
+                        } else if (event.getEventType() == PatternLockCompoundEvent.EventType.PATTERN_CLEARED) {
+                            Log.d(getClass().getName(), "Pattern has been cleared");
                         }
                     }
                 });
